8840415444445ce124748bb5112184719b5320c5
RobertBicknell <robertbicknell@gmail.com>
2020-10-23 14:55:01 +0100

    Initial commit

---
+++ LoginMetricsInterfaces/LoginMetricsRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using LoginMetricsInterfaces;
+
+namespace LoginMetricsInterfaces
+{
+    public interface ILoginMetricsRepo
+    {
+        List<LoginRecord> GetUserLoginsInPeriod(DateTime start, DateTime end, List<string> users);
+    }
+}
